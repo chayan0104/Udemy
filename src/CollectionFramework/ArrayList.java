271e7f4264aa718388cead1a894888640f3adf49
chayan0104 <92307176+chayan0104@users.noreply.github.com>
2021-12-16 17:25:07 +0530

    CollectionFramework

---
+++ src/CollectionFramework/ArrayList.java
@@ -0,0 +1,24 @@
+package CollectionFramework;
+
+public class ArrayList<S> {
+    public static void main(String args[]){
+        //ArrayList<String> alist=new ArrayList<String>();
+
+        java.util.ArrayList<String> alist= new   java.util.ArrayList<String>();
+        alist.add("Steve");
+        alist.add("Tim");
+        alist.add("Lucy");
+        alist.add("Pat");
+        alist.add("Angela");
+        alist.add("Tom");
+
+        //displaying elements
+        System.out.println(alist);
+
+        //Adding "Steve" at the fourth position
+        alist.add(3, "Steve");
+
+        //displaying elements
+        System.out.println(alist);
+    }
+}
---
+++ src/CollectionFramework/Arraylist2.java
@@ -0,0 +1,14 @@
+package CollectionFramework;
+
+public class Arraylist2 {public static void main(String args[]){
+    //ArrayList<String> alist=new ArrayList<String>();
+
+    java.util.ArrayList<Integer> Items= new   java.util.ArrayList<Integer>();
+    Items.add(9);
+    Items.add(6);
+    Items.add(95);
+
+    System.out.println(Items.get(0)+Items.get(2));
+}
+
+}
