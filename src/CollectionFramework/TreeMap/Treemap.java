22c5bac1700e1a6e5e7236e1a977817f0b4efe55
chayan0104 <92307176+chayan0104@users.noreply.github.com>
2022-02-15 10:44:25 +0530

    CollectionFramework

---
+++ src/CollectionFramework/TreeMap/Treemap.java
@@ -0,0 +1,28 @@
+package CollectionFramework.TreeMap;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Treemap {
+    public static void main(String args[]){
+        TreeMap<String, String> dictionary = new TreeMap<String, String>();
+
+        dictionary.put("Brave " , "what you need");
+        dictionary.put("courage" , "what you need");
+        dictionary.put("bravo" , "what you need");
+        dictionary.put("thrill " , "what you need");
+        dictionary.put("mai" , "what you need");
+
+        for(Map.Entry<String,String> entry : dictionary.entrySet()){
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+
+
+
+
+    }
+
+}
